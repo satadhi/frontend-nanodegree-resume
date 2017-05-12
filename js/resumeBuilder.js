6cc0e1e6868d5c037807d6320410aebd9d2acc6b
satadhi <haldersatadhi@gmail.com>
2017-05-12 21:50:34 +0530

    12may17

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Satadhi Halder");
+var formattedRole = HTMLheaderRole.replace("%data%", "web developer");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+var bio = {
+    "name" : "satadhi ",
+    "role" : "web developer ",
+    "contact" : "i am mostly in my home ",
+    "pic" : "../images/fry.jpg"
+    
+}
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.contact);
+$("#main").append(bio.pic);
