dbab6bdab76d4311ca9045843a1abf4f5728a69b
satadhi <haldersatadhi@gmail.com>
2017-09-17 17:41:37 +0530

    17sep2017

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,15 +1,43 @@
-var formattedName = HTMLheaderName.replace("%data%", "Satadhi Halder");
-var formattedRole = HTMLheaderRole.replace("%data%", "web developer");
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+//var formattedName = HTMLheaderName.replace("%data%", "Satadhi Halder");
+//var formattedRole = HTMLheaderRole.replace("%data%", "web developer");
+//$("#header").append(formattedName);
+//$("#header").append(formattedRole);
+var work = {
+  "position" : "totally nothing",
+  "experience" : "1 small project",
+  "years" : "not really"
+};
+
 var bio = {
     "name" : "satadhi ",
     "role" : "web developer ",
     "contact" : "i am mostly in my home ",
-    "pic" : "../images/fry.jpg"
-    
-}
-$("#main").append(bio.name);
-$("#main").append(bio.role);
-$("#main").append(bio.contact);
-$("#main").append(bio.pic);
+    "pic" : "../images/me.jpg"
+
+};
+var education = {
+  "school":[
+      {
+          "name":"Army Public school",
+          "location":"Rajasthan,jodhpur",
+          "degree":"high school"
+      },
+      {
+          "name":"St thomas's Collage of engeneering and technology",
+          "location":"West Bangal,Kolkata",
+          "degree":"B.Tech"
+      }
+
+
+  ]
+};
+var projects = {
+  "projects":"full-stack developer, small time hacker",
+    "title": "there are many small projects that i have made",
+    "dates": 2010
+
+};
+//$("#main").append(bio.name);
+//$("#main").append(bio.role);
+//$("#main").append(bio.contact);
+//$("#main").append(bio.pic);
